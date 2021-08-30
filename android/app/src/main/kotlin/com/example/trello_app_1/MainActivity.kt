b7122743fa225a336e6790f9ff81d877a664bdee
Brandon Herrera <brandon.herrera.work@gmail.com>
2021-08-30 19:41:27 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/trello_app_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.trello_app_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
+
+import 'widgets/drawer widgets/app_drawer.dart';
+import 'widgets/home widgets/home_board_section.dart';
+import './screens/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
